5e039d0ccbf112248b208869e55494e8ce4c5530
Paul Brugger <paul@brugger.com>
2020-07-29 16:58:15 -0600

    remove item

--- src/Table.svelte
+++ src/Table.svelte
@@ -21,6 +21,10 @@
     style: "currency",
     currency: "USD"
   });
+
+  function remove(id) {
+    materialStore.remove(id);
+  }
 </script>
 
 <style>
@@ -29,6 +33,9 @@
   }
   tr {
     cursor: pointer;
+  }
+  tr:last-of-type {
+    cursor: inherit;
   }
 </style>
 
@@ -46,7 +53,9 @@
         <td>{material.name}</td>
         <td>{formatter.format(material.price)}</td>
         <td>
-          <i class="far fa-trash-alt" />
+          <i
+            on:click|stopPropagation={remove(material.id)}
+            class="far fa-trash-alt" />
         </td>
       </tr>
     {/each}
