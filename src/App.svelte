2c52f1275214a5ba85e6b63ec631f5cb07d85ff4
Paul Brugger <paul@brugger.com>
2020-07-29 10:30:01 -0600

    2-form-component

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+  import Form from "./Form.svelte";
+  import Table from "./Table.svelte";
+</script>
+
+<style>
+  main {
+    flex: 1;
+    align-content: center;
+    justify-content: center;
+    width: 80%;
+    margin: 0 auto;
+  }
+</style>
+
+<main>
+  <h1>Project Estimator</h1>
+  <Form />
+  <Table />
+</main>
